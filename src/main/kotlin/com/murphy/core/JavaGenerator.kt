90beb616537d9a4f8765e78b214ce86c324f218e
zhoujin <zhoujin1316@163.com>
2024-07-31 12:01:04 +0800

    遍历

--- src/main/kotlin/com/murphy/core/JavaGenerator.kt
+++ src/main/kotlin/com/murphy/core/JavaGenerator.kt
@@ -10,32 +10,29 @@ import org.jetbrains.kotlin.j2k.isMainMethod
 fun processJava(psi: PsiNamedElement) {
     println("============================== ${psi.name} ==============================")
     val config = AndProguardConfigState.getInstance()
-    val elementSeq = psi.childrenDfsSequence().filterIsInstance<PsiNamedElement>()
-    elementSeq.partition { it is PsiFieldImpl }.run {
-        val skipElements: MutableSet<PsiField> = HashSet()
-        second.reversed().forEach {
-            if (it.isValid.not()) return@forEach
-            when (it) {
-                is PsiAnonymousClassImpl -> return@forEach
-                is PsiClassImpl -> {
-                    it.renameReference()
-                    it.rename(config.randomClassName, "Class")
-                }
-
-                is PsiMethodImpl -> {
-                    val skip = config.skipData && it.isGetterOrSetter()
-                    if (skip) it.getFieldOfGetterOrSetter()?.let { e -> skipElements.add(e) }
-                    if (!skip && it.findSuperMethods().isEmpty() && !it.isConstructor && !it.isMainMethod())
-                        it.rename(config.randomMethodName, "Method")
-                }
-
-                is PsiParameterImpl -> it.rename(config.randomFieldName, "Parameter")
-                is PsiLocalVariableImpl -> it.rename(config.randomFieldName, "Variable")
+    val skipElements: MutableSet<PsiField> = HashSet()
+    psi.childrenDfsSequence().filterIsInstance<PsiMethodImpl>().forEach {
+        val skip = config.skipData && it.isGetterOrSetter()
+        if (skip) it.getFieldOfGetterOrSetter()?.let { e -> skipElements.add(e) }
+    }
+    psi.childrenDfsSequence().filterIsInstance<PsiNamedElement>().toList().reversed().forEach {
+        if (it.isValid.not()) return@forEach
+        when (it) {
+            is PsiAnonymousClassImpl -> return@forEach
+            is PsiClassImpl -> {
+                it.renameReference()
+                it.rename(config.randomClassName, "Class")
             }
-        }
-        first.subtract(skipElements).forEach {
-            if (it.isValid.not()) return@forEach
-            it.rename(config.randomFieldName, "Field")
+
+            is PsiMethodImpl -> {
+                val skip = config.skipData && it.isGetterOrSetter()
+                if (!skip && it.findSuperMethods().isEmpty() && !it.isConstructor && !it.isMainMethod())
+                    it.rename(config.randomMethodName, "Method")
+            }
+
+            is PsiParameterImpl -> it.rename(config.randomFieldName, "Parameter")
+            is PsiLocalVariableImpl -> it.rename(config.randomFieldName, "Variable")
+            is PsiFieldImpl -> if (!skipElements.contains(it)) it.rename(config.randomFieldName, "Field")
         }
     }
 }
