23c5b284b6af7f9b0df59c1a8406d8e8101bdf1f
zhoujin <zhoujin1316@163.com>
2024-07-31 12:01:04 +0800

    调整遍历顺序

--- src/main/kotlin/com/murphy/core/KotlinGenerator.kt
+++ src/main/kotlin/com/murphy/core/KotlinGenerator.kt
@@ -11,44 +11,43 @@ import org.jetbrains.kotlin.psi.*
 fun processKotlin(psi: PsiNamedElement) {
     println("============================== ${psi.name} ==============================")
     val config = AndProguardConfigState.getInstance()
-    val elementSeq = psi.childrenDfsSequence().filterIsInstance<PsiNamedElement>()
-    elementSeq.partition { it is KtParameter }.run {
-        val skipElements: MutableSet<KtParameter> = HashSet()
-        second.reversed().forEach {
-            if (it.isValid.not()) return@forEach
-            when (it) {
-                is KtFile -> {
-                    if (it.classes.size != 1 || it.hasTopLevelCallables())
-                        it.rename(config.randomClassName + KOTLIN_SUFFIX, "File")
-                }
-
-                is KtClass -> {
-                    if (config.skipData && it.isData())
-                        skipElements.addAll(it.primaryConstructorParameters)
-                    it.renameReference()
-                    it.rename(config.randomClassName, "Class")
-                }
-
-                is KtObjectDeclaration -> {
-                    if (!it.isObjectLiteral() && !it.isCompanion())
-                        it.rename(config.randomClassName, "Object Class")
-                }
-
-                is KtNamedFunction -> {
-                    if (!it.hasModifier(KtTokens.OVERRIDE_KEYWORD) && !it.isMainFunction() && !it.isAnonymousFunction)
-                        it.rename(config.randomMethodName, "Function")
-                }
-
-                is KtProperty -> {
-                    if (!it.hasModifier(KtTokens.OVERRIDE_KEYWORD))
-                        it.rename(config.randomFieldName, "Property")
-                }
+    val skipElements: MutableSet<KtParameter> = HashSet()
+    psi.childrenDfsSequence().filterIsInstance<KtClass>().forEach {
+        if (config.skipData && it.isData())
+            skipElements.addAll(it.primaryConstructorParameters)
+    }
+    psi.childrenDfsSequence().filterIsInstance<PsiNamedElement>().toList().reversed().forEach {
+        if (it.isValid.not()) return@forEach
+        when (it) {
+            is KtFile -> {
+                if (it.classes.size != 1 || it.hasTopLevelCallables())
+                    it.rename(config.randomClassName + KOTLIN_SUFFIX, "File")
             }
-        }
-        first.subtract(skipElements).forEach {
-            if (it.isValid.not()) return@forEach
-            if (!(it as KtParameter).hasModifier(KtTokens.OVERRIDE_KEYWORD))
-                it.rename(config.randomFieldName, "Parameter")
+
+            is KtClass -> {
+                it.renameReference()
+                it.rename(config.randomClassName, "Class")
+            }
+
+            is KtObjectDeclaration -> {
+                if (!it.isObjectLiteral() && !it.isCompanion())
+                    it.rename(config.randomClassName, "Object Class")
+            }
+
+            is KtNamedFunction -> {
+                if (!it.hasModifier(KtTokens.OVERRIDE_KEYWORD) && !it.isMainFunction() && !it.isAnonymousFunction)
+                    it.rename(config.randomMethodName, "Function")
+            }
+
+            is KtProperty -> {
+                if (!it.hasModifier(KtTokens.OVERRIDE_KEYWORD))
+                    it.rename(config.randomFieldName, "Property")
+            }
+
+            is KtParameter -> {
+                if (!it.hasModifier(KtTokens.OVERRIDE_KEYWORD) && !skipElements.contains(it))
+                    it.rename(config.randomFieldName, "Parameter")
+            }
         }
     }
 }
